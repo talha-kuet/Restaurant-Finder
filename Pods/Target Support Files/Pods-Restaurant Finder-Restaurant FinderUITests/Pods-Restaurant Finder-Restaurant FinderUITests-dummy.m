8fd8655994a4efaa133fe3c6f65edfa59a5152cf
Musaddique Billah Talha <musaddiquebillahtalha@Musaddiques-MacBook-Pro.local>
2020-03-24 19:20:47 +0600

    Pods removed

--- Pods/Target Support Files/Pods-Restaurant Finder-Restaurant FinderUITests/Pods-Restaurant Finder-Restaurant FinderUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Restaurant_Finder_Restaurant_FinderUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Restaurant_Finder_Restaurant_FinderUITests
-@end
--- Pods/Target Support Files/Pods-Restaurant FinderTests/Pods-Restaurant FinderTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Restaurant_FinderTests : NSObject
-@end
-@implementation PodsDummy_Pods_Restaurant_FinderTests
-@end
